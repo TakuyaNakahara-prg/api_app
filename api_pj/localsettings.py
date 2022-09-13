95c3fec7fb3b23c383c3484285dcfec55097acf4
TakuyaNakahara-prg <gate01221@gmail.com>
2022-09-13 13:07:48 +0000

    ignore

---
+++ api_pj/localsettings.py
@@ -0,0 +1,13 @@
+SECRET_KEY = '5hjw!k89@qmwe4vvromsm@^@u(@e%dv=h=x%!*08_m%tw)ns4y'
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432
+    }
+}
--- api_pj/settings.py
+++ api_pj/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5hjw!k89@qmwe4vvromsm@^@u(@e%dv=h=x%!*08_m%tw)ns4y'
+try:
+    from .localsettings import *
+except ImportError:
+    pass
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -73,16 +76,7 @@ WSGI_APPLICATION = 'api_pj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432
-    }
-}
+
 
 
 # Password validation
